4d0a2871918df7fefc9282ca05a11793cffc0e22
zubair480 <zubairzafar480@gmail.com>
2024-05-15 23:00:29 +0500

    js

---
+++ week 2/day 3/script.js
@@ -0,0 +1,50 @@
+// console.log("Hello world!");
+// let num = 20; // Integer
+// let num2 = 20.001; // Float
+// let message = "Hello this is a beautiful message." // String
+// let message2 = true; // Boolean
+
+// // this is introduction lesson and we have learned data types 
+
+// console.log(num);
+
+// console.log(num2);
+
+// console.log(message);
+
+// console.log(message2); // ctrl + forward slash (/) to comment a line
+
+
+
+// Arithematic Operators 
+
+// console.log(2 + 2) // addition 
+// console.log("2 + 2") // string
+
+// console.log(2 - 2) // subtraction
+
+// console.log(2 * 2) // multiplication
+
+// console.log(2 / 2) // division
+
+// let num = 2; 
+// let num2 = 20;
+
+// console.log(num + num2)
+
+// let num = 2; 
+// let num2 = 20;
+// let sum = num + num2
+
+// console.log(sum)
+
+
+// Assignment Operators
+
+let num = 2;
+num = num + 5
+console.log(num)
+
+
+
+
